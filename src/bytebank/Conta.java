17f8a37a83aaf57beb10969f818f5bcde32a892f
LuanTedesco <luanfelipe2040@gmail.com>
2022-06-16 19:45:35 -0300

    Atualização 16/06

--- src/bytebank/Conta.java
+++ src/bytebank/Conta.java
@@ -5,5 +5,18 @@ public class Conta {
   double saldo;
   int agencia;
   int numero;
-  String titular;
+  Cliente titular;
+
+  public void deposita(double valor) {
+    this.saldo = this.saldo + valor;
+  }
+
+  public boolean saca(double valor) {
+    if (this.saldo >= valor) {
+      this.saldo = this.saldo - valor;
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
